7d2a00924b3fbca935dfe5119a6cbc4657495510
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-04-29 12:06:15 +0200

    Add new activity and button

---
+++ app/src/main/java/com/example/alex/hourcalculator/AddHoursActivity.java
@@ -0,0 +1,13 @@
+package com.example.alex.hourcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddHoursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hours);
+    }
+}
