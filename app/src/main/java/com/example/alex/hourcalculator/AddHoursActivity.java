64fe918ac14fc857b3a555fe1df4b50357df9428
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-04-29 14:18:17 +0200

    Add time picker

--- app/src/main/java/com/example/alex/hourcalculator/AddHoursActivity.java
+++ app/src/main/java/com/example/alex/hourcalculator/AddHoursActivity.java
@@ -1,20 +1,90 @@
 package com.example.alex.hourcalculator;
 
+import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddHoursActivity extends AppCompatActivity {
+public class AddHoursActivity extends AppCompatActivity implements TimePickerFragment.TimePickedListener{
+
+    private boolean isStartHour = false;
+
+    int startHour;
+    int startMinutes;
+    int endHour;
+    int endMInutes;
+
+    EditText startHourtext;
+    EditText endHourText ;
+    EditText finalHourtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_hours);
+
+        //Set default time
+        startHour = 9;
+        startMinutes = 0;
+        endHour = 18;
+        endMInutes = 0;
+
+        Button setStartHour = (Button) findViewById(R.id.setStartHourBtn);
+        startHourtext = (EditText) findViewById(R.id.startTimeTxt);
+        endHourText = (EditText) findViewById(R.id.endTimeTxt);
+        finalHourtext = (EditText) findViewById(R.id.finalTimeTxt);
+
+        setStartHour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isStartHour = true;
+                showTimePickerDialog(v);
+            }
+        });
+
+        Button setEndHour = (Button) findViewById(R.id.setEndHourBtn);
+        setEndHour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isStartHour = false;
+                showTimePickerDialog(v);
+            }
+        });
+
+        Button saveHours = (Button) findViewById(R.id.saveHours);
+        saveHours.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
     }
 
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
+        Bundle bundle = new Bundle();
+        if(isStartHour){
+            bundle.putInt("hour", startHour);
+            bundle.putInt("minutes", startMinutes);
+        }
+        else {
+            bundle.putInt("hour", endHour);
+            bundle.putInt("minutes", endMInutes);
+        }
+        newFragment.setArguments(bundle);
         newFragment.show(getSupportFragmentManager(), "time picker");
+    }
+
+    @Override
+    public void onTimePicked(int hour, int minutes) {
+        if(isStartHour){
+            startHourtext.setText(Integer.toString(hour) + ": " + Integer.toString(minutes));
+            startHour = hour;
+            startMinutes = minutes;
+        }
+        else{
+            endHourText.setText(Integer.toString(hour) + ": " + Integer.toString(minutes));
+            endHour = hour;
+            endMInutes = minutes;
+        }
     }
 }
--- app/src/main/java/com/example/alex/hourcalculator/TimePickerFragment.java
+++ app/src/main/java/com/example/alex/hourcalculator/TimePickerFragment.java
@@ -2,32 +2,48 @@ package com.example.alex.hourcalculator;
 
  import android.app.Dialog;
  import android.app.TimePickerDialog;
+ import android.content.Context;
  import android.os.Bundle;
+ import android.support.v7.widget.ContentFrameLayout;
  import android.text.format.DateFormat;
  import android.widget.TimePicker;
  import android.widget.Toast;
  import java.util.Calendar;
  import android.support.v4.app.DialogFragment;
 
-public class TimePickerFragment extends DialogFragment {
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+    private TimePickedListener listener;
+
+    public static interface TimePickedListener {
+        void onTimePicked(int hour, int minutes);
+    }
+
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+
+        try {
+            listener = (TimePickedListener) activity;
+
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnDateSetListener.");
+        }
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        
+        int hour = getArguments().getInt("hour");
+        int minute = getArguments().getInt("minutes");
 
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
-
-        return new TimePickerDialog(getActivity(), timeSetListener, hour, minute,
+        return new TimePickerDialog(getActivity(), this, hour, minute,
                 DateFormat.is24HourFormat(getActivity()));
     }
 
-    private TimePickerDialog.OnTimeSetListener timeSetListener =
-            new TimePickerDialog.OnTimeSetListener() {
-                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                    Toast.makeText(getActivity(), "selected time is "
-                                    + view.getCurrentHour() +
-                                    " / " + view.getCurrentMinute()
-                            , Toast.LENGTH_SHORT).show();
-                }
-            };
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        listener.onTimePicked(hourOfDay,minute);
+    }
 }
